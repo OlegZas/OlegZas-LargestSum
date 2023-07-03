887e8a96a5da42c8b80fe202068106a5b63c7fdd
Oleg Zasukha <ozamericos@gmail.com>
2023-07-03 18:48:13 +0000

    Largest sum of 2 numbers from the list

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,16 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        int max = 0; 
+        int min2 = 0; 
+        for (int i = 0; i < nums.size();i++ ){
+            if (nums.get(i) > max){
+                min2 = max; // min2 is going to be the second largest since it will equal max before the final i is compared. 
+                max = nums.get(i); 
+            } else if (nums.get(i)> min2){
+                min2 = nums.get(i); 
+            }
+        }
+        return max + min2;
     }
 }
